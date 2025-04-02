4c9d5f95bb31e1ab11185db66253510d01c818cc
Sheila-Salles <sheilacsalles@hotmail.com>
2025-04-02 09:42:31 -0300

    atualizacao

--- src/Pages/Doados/doados.jsx
+++ src/Pages/Doados/doados.jsx
@@ -1,29 +1,45 @@
-import S from '../Doados/doados.module.scss'
-import livros from '../../assets/livros.png'
+import S from "../Doados/doados.module.scss";
+import livros from "../../assets/livros.png";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
+export default function Doados() {
+  const [livros, setLivros] = useState([]);
 
-export default function Doados(){
-    return(
-        <section className={S.boxDoados}>
-            <h2>Livros Doados</h2>
-            <section className={S.boxcard}>
-                <article>
-                    <img src={livros} alt="imagem de um livro que se chama o protagonista" />
+  const getLivros = async () => {
+    const response = await axios.get(
+      "https://projeto-api-6-n4e8.onrender.com/livros"
+    );
+    setLivros(response.data);
+  };
 
-                    <h3>O Protagonista</h3>
-                    <p>Susanne Andrade</p>
-                    <p>Ficção</p>
+  useEffect(() => {
+    getLivros();
+  }, []);
 
-                </article>
-                <article>
-                    <img src={livros} alt="imagem de um livro que se chama o protagonista" />
-                    
-                    <h3>O Protagonista</h3>
-                    <p>Susanne Andrade</p>
-                    <p>Ficção</p>
-                    
-                </article>
-            </section>
-        </section>
-    )
+  return (
+    <section className={S.boxDoados}>
+      <h2>Livros Doados</h2>
+      <section className={S.boxcard}>
+        <article>
+          <img
+            src={livros}
+            alt="imagem de um livro que se chama o protagonista"
+          />
+
+          <h3>O Protagonista</h3>
+          <p>Susanne Andrade</p>
+          <p>Ficção</p>
+        </article>
+        {livros.map((item) => (
+          <article key={item.id}>
+            <img src={item.imagem_url} alt="" />
+            <h3>{item.titulo}</h3>
+            <p>{item.categoria}</p>
+            <p>{item.autor}</p>
+          </article>
+        ))}
+      </section>
+    </section>
+  );
 }
